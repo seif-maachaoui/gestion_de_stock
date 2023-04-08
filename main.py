885f108f61a64219dc45cebe3c65ca6394bce253
seif-maachaoui <seifeddine.maachaoui@laplateforme.io>
2023-04-08 14:29:04 +0200

    Veuillez éxecuter le code depuis le fichier principal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Importation des modules requis
 from interface import *
 
-#Veuillez éxecuter le code depuis le fichier main
+#L'éxecution du code se fait ici
 
 #Initialisation d'un objet Interface
 #Puis j'apelle les différentes méthode qui constituent mon Interface
